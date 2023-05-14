9f79ed747f436b0b7c12e6f8da7eec35e44e5cb9
Kacper Łukaszczyk <k.lukaszczyk@gmail.com>
2023-05-14 12:21:53 +0200

    Add products to cart

--- dist/js/script.js
+++ dist/js/script.js
@@ -156,6 +156,7 @@ const templates = {
       thisProduct.cartButton.addEventListener('click', function (event) {
         event.preventDefault();
         thisProduct.processOrder();
+        thisProduct.addToCart();
       });
     }
     processOrder() {
@@ -219,7 +220,10 @@ const templates = {
         price *= thisProduct.amountWidget.value;
 
         // update calculated price in the HTML
+        thisProduct.priceSingle = price;
         thisProduct.priceElem.innerHTML = price;
+        thisProduct.price = price;
+
       }
     }
     initAmountWidget(){
@@ -231,6 +235,57 @@ const templates = {
       });
     }
 
+    addToCart(){
+      const thisProduct = this;
+
+      app.cart.add(thisProduct.prepareCartProduct());
+    }
+
+    prepareCartProduct(){
+      const thisProduct = this;
+      const productSummary = {
+        id: thisProduct.id,
+        name: thisProduct.data.name,
+        amount: thisProduct.amountWidget.value,
+        priceSingle: thisProduct.priceSingle,
+        price: thisProduct.price,
+        params: thisProduct.prepareCartProductParams(),
+      };
+      return productSummary;
+    }
+
+    prepareCartProductParams() {
+      const thisProduct = this;
+
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      const params = {};
+    
+      // for very category (param)
+      for(let paramId in thisProduct.data.params) {
+        const param = thisProduct.data.params[paramId];
+    
+        // create category param in params const eg. params = { ingredients: { name: 'Ingredients', options: {}}}
+        params[paramId] = {
+          label: param.label,
+          options: {}
+        };
+    
+        // for every option in this category
+        for(let optionId in param.options) {
+          const option = param.options[optionId];
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+    
+          if(optionSelected) {
+            // option is selected!
+            params[paramId].options[optionId] = option.label;
+
+          }
+        }
+      }
+    
+      return params;
+    }
+
   }
 
 class AmountWidget {
@@ -239,8 +294,8 @@ class AmountWidget {
     thisWidget.getElements(element);
     thisWidget.setValue(settings.amountWidget.defaultValue);
     thisWidget.initActions();
-    console.log('AmountWidget:', thisWidget);
-    console.log('constructor arguments:', element);
+    //console.log('AmountWidget:', thisWidget);
+    //console.log('constructor arguments:', element);
   }
 
   getElements(element){
@@ -308,6 +363,7 @@ class Cart {
     thisCart.dom = [];
     thisCart.dom.wrapper = element;
     thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    thisCart.dom.productList = thisCart.dom.wrapper.querySelector(select.cart.productList);
   }
 
   initActions(){
@@ -316,7 +372,18 @@ class Cart {
       thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
     });
   }
-}
+
+  add(menuProduct) {
+    const thisCart = this;
+    const generatedHTML = templates.cartProduct(menuProduct); 
+    const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+    thisCart.dom.productList.appendChild(generatedDOM);
+ 
+    }
+
+    //console.log('adding product', menuProduct);
+  }
+
 
   const app = {
     initMenu: function () {
